e5d3527ed243a16b0b9f6b7f194c35ed0ec215bf
unknown <daffadhiya50@gmail.com>
2022-07-11 21:29:18 +0700

    class

--- classList.py
+++ classList.py
@@ -4,14 +4,4 @@ def readFile():
     WORDS = FILE_OBJECT.read().splitlines()
     FILE_OBJECT.close()
 
-    return 
-    
-def pred(FILE):
-    SHAPE = (224, 224)
-    IMG = plt.imread(FILE)
-    IMG = cv2.resize(IMG,(224,224))
-    IMG = IMG.reshape(1,224,224,3)
-
-    PRED = MODEL.predict(np.array(IMG))
-
-    return CLASS[np.argmax(PRED)]
+    return WORDS
