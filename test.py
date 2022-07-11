9a93d354cde576d5cfe6170ca5f5f4d572cee492
unknown <daffadhiya50@gmail.com>
2022-07-11 20:26:11 +0700

    test

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
 from classList import readFile
 
 from flask import Flask, render_template, request
-import numpy as np 
-import cv2
-import matplotlib.pyplot as plt
 import socket
 
 hostname = socket.gethostname()
@@ -14,7 +11,7 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
 @app.route('/')
 def index():
     return render_template('index.html')
-    
+
 @app.route('/upload')
 def upload():
     return render_template('upload.html')
@@ -23,18 +20,12 @@ def upload():
 def prediction():
     FILE = request.files['file1']
     FILE.save('static/temp_file_'+hostname+'.jpg')
-    #PRED = pred(FILE)
-    PRED = 'tester'
     DATA = {
-        'hostname': hostname,
-        'pred': PRED
+        'hostname': 'hostname',
+        'pred': 'test'
     }
 
     return render_template('predict.html', data = DATA)
-
-if __name__ == '__main__':
-    app.run()
-
 
 if __name__ == '__main__':
     app.run()
